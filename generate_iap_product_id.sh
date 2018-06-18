2982e4bf4a7302bfcb12e7ae8beba7f8fff367bd
alexchow <cplover@live.com>
2018-06-19 00:09:05 +0800

    update

--- generate_iap_product_id.sh
+++ generate_iap_product_id.sh
@@ -4,16 +4,16 @@ if [ ! -n "$1" ] ;then
 else
     echo 'Member IAP Product Id:'
     echo ''
-    echo "Premium 1 month:  $1.iap.member.2.2592000"
-    echo "Premium 2 month:  $1.iap.member.2.5184000"
-    echo "Premium 3 month:  $1.iap.member.2.7776000"
-    echo "Premium 6 month:  $1.iap.member.2.15552000"
-    echo "Premium 12 month: $1.iap.member.2.31104000"
+    echo "1-month Premium:  $1.iap.member.2.2592000"
+    echo "2-month Premium:  $1.iap.member.2.5184000"
+    echo "3-month Premium:  $1.iap.member.2.7776000"
+    echo "6-month Premium:  $1.iap.member.2.15552000"
+    echo "12-month Premium: $1.iap.member.2.31104000"
     echo ''
-    echo "Advanced 1 month:  $1.iap.member.3.2592000"
-    echo "Advanced 2 month:  $1.iap.member.3.5184000"
-    echo "Advanced 3 month:  $1.iap.member.3.7776000"
-    echo "Advanced 6 month:  $1.iap.member.3.15552000"
-    echo "Advanced 12 month: $1.iap.member.3.31104000"
+    echo "1-month Advanced:  $1.iap.member.3.2592000"
+    echo "2-month Advanced:  $1.iap.member.3.5184000"
+    echo "3-month Advanced:  $1.iap.member.3.7776000"
+    echo "6-month Advanced:  $1.iap.member.3.15552000"
+    echo "12-month Advanced: $1.iap.member.3.31104000"
     echo ''
 fi
